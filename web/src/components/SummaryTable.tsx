f8f71ebfa3273d3d13335ac9612a00c4b4dd99a7
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2023-02-07 01:08:44 -0300

    geração de range de dias e preenchimento dos dias no fim

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,3 +1,4 @@
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beggining"
 import { HabitDay } from "./HabitDay"
 
 const weekDays = [
@@ -9,6 +10,11 @@ const weekDays = [
     'S',
     'S',
 ]
+
+const summaryDates = generateDatesFromYearBeginning()
+
+const minimumSummaryDatesSize = 18 * 7
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
 
 export function SummaryTable() {
     return (
@@ -27,21 +33,15 @@ export function SummaryTable() {
             </div>
 
             <div className="grid grid-rows-7 grid-flow-col gap-3">
-                <HabitDay/>
-                <HabitDay/>
-                <HabitDay/>
-                <HabitDay/>
-                <HabitDay/>
-                <HabitDay/>
-                <HabitDay/>
-                <HabitDay/>
-                <HabitDay/>
-                <HabitDay/>
-                <HabitDay/>
-                <HabitDay/>
-                <HabitDay/>
-                <HabitDay/>
-                <HabitDay/>
+                {summaryDates.map((date, index) => {
+                    return <HabitDay key={index} />
+                })}
+
+                {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, index) => {
+                    return (
+                        <div key={index} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
+                    )
+                })}
             </div>
         </div>
     )
